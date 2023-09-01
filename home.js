3ae09d78c947f9b1d3327cbd422d6c760c1d6887
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-09-02 01:34:24 +0530

    home page done

--- home.js
+++ home.js
@@ -24,25 +24,107 @@ header.innerHTML = `
 
 
 `
+var banner = document.getElementsByClassName("banner")[0]; 
+banner.innerHTML = `
+<div class="car">
+<div id="carousel">
+    <img src="https://shoplane.netlify.app/img/img4.png" alt="Image 1">
+    <img src="https://shoplane.netlify.app/img/img1.png" alt="Image 2">
+    <img src="https://shoplane.netlify.app/img/img2.png" alt="Image 3">
+    <img src="https://shoplane.netlify.app/img/img3.png" alt="Image 4">
+</div>
+</div>
+<div class="dots">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+</div>
+`;
+
 const carousel = document.getElementById('carousel');
 const images = carousel.getElementsByTagName('img');
+const dots = document.querySelectorAll('.dot');
 let currentIndex = 0;
+
+function showImage(index) {
+    currentIndex = index;
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+    updateDots();
+}
+
+function updateDots() {
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        showImage(index);
+    });
+});
 
 function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
-    const offset = -currentIndex * 100; // 100% width for each image
+    const offset = -currentIndex * 100;
     carousel.style.transform = `translateX(${offset}%)`;
-
-    if (currentIndex === images.length - 1) {
-        setTimeout(() => {
-            currentIndex = 0;
-            carousel.style.transition = 'none';
-            carousel.style.transform = `translateX(0%)`;
-            setTimeout(() => {
-                carousel.style.transition = 'transform 1s ease';
-            }, 0);
-        }, 1000);
-    }
+    updateDots();
 }
 
 setInterval(nextImage, 2000);
+
+
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://5d76bf96515d1a0014085cf9.mockapi.io/product',
+        type: 'GET',
+        success: function(response) {
+            // Loop through the API data and display product cards
+            for (var i = 0; i < response.length; i++) {
+                var product = response[i];
+
+                var cardElement = document.createElement("div");
+                cardElement.className = "cloth-card";
+                var cardImage = document.createElement("img");
+                cardImage.className = "card-img";
+                var cardContent = document.createElement("div");
+                cardContent.className = "card-content";
+
+                var cardDescription = document.createElement("h3");
+                cardDescription.className = "card-desc";
+                var cardBrand = document.createElement("h4");
+                cardBrand.className = "card-brand";
+                var cardPrice = document.createElement("h5");
+                cardPrice.className = "card-price";
+
+                cardImage.src = product.preview;
+                cardDescription.innerText = product.name;
+                cardBrand.innerText = product.brand;
+                cardPrice.innerText = "Rs " + product.price;
+
+                if (product.isAccessory) {
+                    // Add to accessories section
+                    document.getElementById("accessories-section").appendChild(cardElement);
+                } else {
+                    // Add to clothing section
+                    document.getElementById("clothing-section").appendChild(cardElement);
+                }
+
+                cardElement.appendChild(cardImage);
+                cardElement.appendChild(cardContent);
+                cardContent.appendChild(cardDescription);
+                cardContent.appendChild(cardBrand);
+                cardContent.appendChild(cardPrice);
+            }
+        },
+        error: function(error) {
+            console.log('Error:', error);
+        }
+    });
+});
